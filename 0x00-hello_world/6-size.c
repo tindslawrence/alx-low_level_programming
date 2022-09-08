903c78045fe6a16f2f3c514c12edf7cca93c2086
Lawrence Tindyebwa <tindslawrence@gmail.com>
2022-09-08 15:49:23 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-
-
 /**
  *
  * * main - print the string in the put function
  *
- * *
+ *
  *
  * * Description: using the main function
  *
@@ -14,7 +12,7 @@
  *
  * * Return: 0
  *
- * */
+ */
 
 int main(void)
 
