eed9d04d52863f84c7549c5f463b55e25d8d34dc
Lawrence Tindyebwa <tindslawrence@gmail.com>
2022-09-08 14:14:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -14,12 +14,9 @@ int main(void)
 
 {
 
-		char Mystr[] = "\"Programming is like building a multilingual puzzle";
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
 
-
-
-			puts(Mystr);
-
-				return (0);
+puts(Mystr);
+return (0);
 
 }
