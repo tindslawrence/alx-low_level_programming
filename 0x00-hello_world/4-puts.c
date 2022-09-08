e326954ff87f591ee04fb13d745e933e0f819f81
Lawrence Tindyebwa <tindslawrence@gmail.com>
2022-09-08 13:38:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdion.h>
+#include <stdio.h>
 /**
- * main . print to screen
- *
- * Return: zero
- */
+* main - print to screen
+* Return: zero
+*/
 int main(void)
 {
 char Mystr[] -"\"Programming is like building a multilingual puzzle";
+
 puts(Mystr);
 return (0);
 }
