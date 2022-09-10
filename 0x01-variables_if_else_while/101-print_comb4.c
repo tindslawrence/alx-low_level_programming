8f2d275d509d279da4c7e1c976ac619260fe60b9
Lawrence Tindyebwa <tindslawrence@gmail.com>
2022-09-10 00:23:27 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,22 +7,19 @@
 int main(void)
 {
     int i, p, m;
-
     for (i = 0; i <= 9; i++)
-    {
-        for (p = 1; p <= 9; p++)
         {
+        for (p = 1; p <= 9; p++)
+                {
             for (m = 2; m <= 9; m++)
-
             {
                 if (m > p && p > i)
-
                 {
                     putchar(i + '0');
                     putchar(p + '0');
                     putchar(m + '0');
                     if (i != 7)
-                    {
+                                        {
                         putchar(',');
                         putchar(' ');
                     }
