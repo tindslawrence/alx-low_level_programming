cd06e342c09ed82689b70dce5daa59b345c16640
Lawrence Tindyebwa <tindslawrence@gmail.com>
2022-09-13 07:40:12 -0700

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - debugging example
+ * Return: 0
+ */
+int main(void)
+{
+    char *main = "Main";
+    while (*main)
+    {
+        _putchar(*main);
+        main++;
+    }
+    putchar('\n');
+    retun (0)
+}
