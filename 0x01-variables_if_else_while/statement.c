37feca166ca26913c17a3831f3b167bd2f9e1679
Lawrence Tindyebwa <tindslawrence@gmail.com>
2022-09-17 08:35:58 -0700

    c-if-else statement.c

---
+++ 0x01-variables_if_else_while/statement.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+  int a = 100;
+  if (a < 20 )
+    {
+      printf("a is less than 20\n");
+    }
+  else
+    {
+      printf("a is not less than 20\n");
+    }
+  printf("value of a is: %d\n", a);
+  return 0;
+}
