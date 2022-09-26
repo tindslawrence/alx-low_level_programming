440651f9a33510c346b445790f8580534a2b5914
Lawrence Tindyebwa <tindslawrence@gmail.com>
2022-09-26 11:56:03 -0700

    Write a function that copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,23 +1,18 @@
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * *_memcpy -  copies memory area from src to dest                                                                                      
- * @dest: destination                                                                                                                   
- * @src: source                                                                                                                         
- * @n: max bytes to use                                                                                                                 
- * Return: dest                                                                                                                         
- */                                                                                                                                     
-                                                                                                                                        
-                                                                                                                                        
-char *_memcpy(char *dest, char *src, unsigned int n)                                                                                    
-{                                                                                                                                       
-        unsigned int i;                                                                                                                 
-                                                                                                                                        
-        for (i = 0; n > 0 ; i++, n--)                                                                                                   
-        {                                                                                                                               
-                dest[i] = src[i];                                                                                                       
-        }                                                                                                                               
-                                                                                                                                        
-        return (dest);                                                                                                                  
-}
+/**
+ * _memcpy - copies n bytes from source memory to destination
+ *
+ * @dest: destination to write to
+ * @src: source to copy from
+ * @n: number of bytes to write
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, int n)
+{
+	char *ptr = dest;
 
+	while (n--)
+		*ptr++ = *src++;
+
+	return (dest);
+}
